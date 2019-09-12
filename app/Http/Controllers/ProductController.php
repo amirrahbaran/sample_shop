70fb1563f2f55a9568b6d7bf1a2af0652265292f
AmirReza Rahbaran <amirrezarahbaran@gmail.com>
2019-09-12 04:50:56 +0430

    Handle the ModelNotFoundException by FindOrFail

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -14,7 +14,7 @@ class ProductController extends Controller
 
     public function show($id)
     {
-        $product = Product::find($id);
+        $product = Product::findOrFail($id);
         return view('products.show', compact('product'));
     }
 }

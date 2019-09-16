57bcb90caefdc297f6ae49e93a09bbac5162a744
amirrahbaran <amirrezarahbaran@gmail.com>
2019-09-16 14:43:04 +0430

    Add Blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,8 +1,12 @@
-<h1>
-    Users are listed as follows:
-    <ul>
-        @foreach($users as $user)
-            <li>{{$user['firstName']}} {{$user['lastName']}}</li>
-        @endforeach
-    </ul>
-</h1>
+@extends('layouts.app')
+
+@section('content')
+    <h1>
+        Users are listed as follows:
+        <ul>
+            @foreach($users as $user)
+                <li>{{$user['firstName']}} {{$user['lastName']}}</li>
+            @endforeach
+        </ul>
+    </h1>    
+@endsection

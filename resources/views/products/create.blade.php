6aff56cb60df99ec6235e51b2fdb316e064825ce
amirrahbaran <amirrezarahbaran@gmail.com>
2019-09-17 13:27:26 +0430

    Added Including Sub-Views

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -3,42 +3,46 @@
 @section('content')
     <div class="card">
         <div class="card-body">
+
             <h5 class="card-title">
                 Create Product
             </h5>
+
             <div class="card-text">
+
                 <form method="POST" action="{{ route('products.store') }}">
+
                     {{ csrf_field() }}
+
                     <div class="form-group">
                         <label for="name">Name</label>
-                    <input type="text" class="form-control" id="name" name="name" value="{{ old('name') }}" placeholder="Enter product name">
+                        <input type="text" class="form-control" id="name" name="name" value="{{ old('name') }}" placeholder="Enter product name">
                     </div>
+
                     <div class="form-group">
                         <label for="description">Description</label>
                         <input type="text" class="form-control" id="description" name="description" value="{{ old('description') }}" placeholder="Enter product description">
                     </div>
+
                     <div class="form-group">
                         <label for="weight">Weight</label>
                         <input type="number" class="form-control" id="weight" name="weight" value="{{ old('weight') }}" placeholder="Enter product weight">
                     </div>
+
                     <div class="form-group">
                         <label for="price">Price</label>
                         <input type="number" class="form-control" id="price" name="price" value="{{ old('price') }}" placeholder="Enter product price">
                     </div>
 
-                    @if ($errors->any())
-                        <div class="alert alert-danger">
-                            <ul>
-                                @foreach ($errors->all() as $error)
-                                    <li>{{ $error }}</li>
-                                @endforeach
-                            </ul>
-                        </div>
-                    @endif
-
+                    @include('shared.errors')
+                    
                     <button type="submit" class="btn btn-success">Save</button>
+
                 </form>
+
             </div>
+
         </div>
+
     </div>
 @endsection

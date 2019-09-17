38fa9734a134b936c3f62dbddaf88b126010744f
amirrahbaran <amirrezarahbaran@gmail.com>
2019-09-17 12:11:43 +0430

    Add simple validation

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -11,20 +11,31 @@
                     {{ csrf_field() }}
                     <div class="form-group">
                         <label for="name">Name</label>
-                        <input type="text" class="form-control" id="name" name="name" placeholder="Enter product name">
+                    <input type="text" class="form-control" id="name" name="name" value="{{ old('name') }}" placeholder="Enter product name">
                     </div>
                     <div class="form-group">
                         <label for="description">Description</label>
-                        <input type="text" class="form-control" id="description" name="description" placeholder="Enter product description">
+                        <input type="text" class="form-control" id="description" name="description" value="{{ old('description') }}" placeholder="Enter product description">
                     </div>
                     <div class="form-group">
                         <label for="weight">Weight</label>
-                        <input type="number" class="form-control" id="weight" name="weight" placeholder="Enter product weight">
+                        <input type="number" class="form-control" id="weight" name="weight" value="{{ old('weight') }}" placeholder="Enter product weight">
                     </div>
                     <div class="form-group">
                         <label for="price">Price</label>
-                        <input type="number" class="form-control" id="price" name="price" placeholder="Enter product price">
+                        <input type="number" class="form-control" id="price" name="price" value="{{ old('price') }}" placeholder="Enter product price">
                     </div>
+
+                    @if ($errors->any())
+                        <div class="alert alert-danger">
+                            <ul>
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endif
+
                     <button type="submit" class="btn btn-success">Save</button>
                 </form>
             </div>
